bf33b3bb52971d32d8d7343dcce7392fe0228998
Henry <63872314+Fingolfin7@users.noreply.github.com>
2025-03-08 19:47:57 +0100

    small typo broke the versioning query in the head tags. fixed it. (?v not v?)

--- core/context_processors.py
+++ core/context_processors.py
@@ -6,32 +6,26 @@ from django.conf import settings
 
 def static_version(request):
     version = {}
-    static_files = []
 
+    # Use STATIC_ROOT in production, otherwise look in app's static directory
+    base_static = settings.STATIC_ROOT if not settings.DEBUG else os.path.join("core", "static")
 
-    js_dir = os.path.join("core/static/core/js")
-    for file in os.listdir(js_dir):
-        file_path = os.path.join(js_dir, file)
-        static_files.append(file_path)
+    # Define directories to scan
+    static_dirs = {
+        'js': os.path.join(base_static, "core", "js"),
+        'css': os.path.join(base_static, "core", "css"),
+    }
 
-    css_dir = os.path.join("core/static/core/css")
-    for file in os.listdir(css_dir):
-        file_path = os.path.join(css_dir, file)
-        static_files.append(file_path)
+    print("Static directories: ", static_dirs)
 
-    # img_dir = os.path.join("core/static/core/images")
-    # for file in os.listdir(img_dir):
-    #     file_path = os.path.join(img_dir, file)
-    #     static_files.append(file_path)
-
-
-    for file in static_files:
-        # get base name of the file
-        base_name = os.path.basename(file)
-        if os.path.exists(file):
-            version[base_name] = int(os.path.getmtime(file))
-        else:
-            version[base_name] = 0
+    # Scan each directory
+    for dir_path in static_dirs.values():
+        if os.path.exists(dir_path):
+            for file in os.listdir(dir_path):
+                file_path = os.path.join(dir_path, file)
+                if os.path.isfile(file_path):
+                    basename = os.path.basename(file_path)
+                    version[basename] = int(os.path.getmtime(file_path))
 
     if settings.DEBUG:
         print("Static version: ", json.dumps(version, indent=4))
