3629dd5e0b0f9324e20b8202ca6230bebbeddd5e
Henry <63872314+Fingolfin7@users.noreply.github.com>
2025-06-19 01:38:05 +0200

    FIxed logout but (turns out it was because i was manually specifiying the http methods for the logout in urls.py)
    Refactor logout functionality: replace link with a form for improved security and user experience

--- AutumnWeb/urls.py
+++ AutumnWeb/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('login/', user_views.CustomLoginView.as_view(template_name='users/login.html',
                                                  authentication_form=UserLoginForm), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html', http_method_names=['get', 'post']), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/', user_views.profile, name='profile'),
 
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html',
